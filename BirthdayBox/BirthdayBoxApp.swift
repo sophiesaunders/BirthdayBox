7b05b866b3198da15f52dc03616687680b6697bb
sophiesaunders <81704139+sophiesaunders@users.noreply.github.com>
2026-07-05 17:34:30 -0600

    Initial Commit

---
+++ BirthdayBox/BirthdayBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayBoxApp.swift
+//  BirthdayBox
+//
+//  Created by Sophie Saunders on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
